ada61e6fc59ba0eaaebb83024e45eb45e95b74a1
bilalaziz-triotech <bilal.aziz@triotechsystems.com>
2026-01-12 00:50:31 +0500

    fixes

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 python manage.py fix_db_sequences
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py fix_db_sequences
 gunicorn --bind=0.0.0.0 --timeout 600 socialapp.wsgi
